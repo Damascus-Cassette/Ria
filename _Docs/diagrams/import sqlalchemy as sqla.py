35dca0b53b7028c40e7d4fc04e6da6055961095e
ClayOfMan <166956516+ClayOfMan@users.noreply.github.com>
2025-06-26 17:45:03 -0400

    cont working on db

--- _Docs/diagrams/import sqlalchemy as sqla.py
+++ _Docs/diagrams/import sqlalchemy as sqla.py
@@ -3,40 +3,99 @@ from sqlalchemy import base as sqlbase
 
 
 
-class fm_db_file(sqlbase):
-    ''' Hash as UUID based file entry '''
-    __tablename__ = 'files'
-    id     = sqla.Column(primary_key = True)
-    #names
-    spaces = sqla.relationship('spaces',uselist=True,backref='spaces')
+sqla.Table()
     
+space_file = sqla.Table('SpaceFile',
+    sqla.Column('id'            , sqla.Integer, primary_key=True), #Make UUID?
+    sqla.Column('itemName'      , sqla.String),
+    sqla.Column('parentSpaceId' , sqla.String, sqla.ForeignKey('spaces.id')),
+    sqla.Column('fileId'        , sqla.String, sqla.ForeignKey('files.id') ,backref='asNames'),
+)
+
+space_space = sqla.Table('SpaceSpace',
+    sqla.Column('id'            , sqla.Integer, primary_key=True), #Make UUID?
+    sqla.Column('itemName'      , sqla.String),
+    sqla.Column('parentSpaceId' , sqla.String, sqla.ForeignKey('spaces.id')),
+    sqla.Column('childSpaceId'  , sqla.String, sqla.ForeignKey('spaces.id'),backref='asNames'),
+)
+
 class fm_db_space(sqlbase):
     ''' Container of named files and named sub-spaces '''
     __tablename__ = 'spaces'
-    id     = sqla.Column(primary_key = True)
-    #names
-    spaces = sqla.relationship('namedspace',uselist=True,backref='spaces')
-    files  = sqla.relationship('namedfile' ,uselist=True,backref='files')
+    id     = sqla.Column(sqla.String, primary_key = True) #Hash of object
+
+    spaces = sqla.relationship('namedspace', secondary=space_space, backref='pSpace')
+    files  = sqla.relationship('namedfile' , secondary=space_file,  backref='pSpace')
+
+    #asNames is backref?
+    inSpaces = sqla.relationship('spaces', sqla.ForeignKey('asNames.parentSpaceId') , uselist=True) 
+
+    #inExports
+    #inSessions
+
+
+class fm_db_file(sqlbase):
+    ''' Hash as UUID based file entry '''
+    __tablename__ = 'files'
+    id     = sqla.Column(sqla.String, primary_key = True) #Hash of object
     
-
-class rel_namedfile():
-    __tablename__ = 'namedfile'
-    space_user = 
-    name   = sqla.Column(sqla.String)    
-    target = sqla.relationship('files' ,backref='names')
-
-class rel_namedspace():
-    __tablename__ = 'namedspace'
-    target = sqla.relationship('spaces',backref='names')    
-    name   = sqla.Column(sqla.String)    
-    
+    #asNames is backref?
+    inSpaces = sqla.relationship('spaces', sqla.ForeignKey('asNames.parentSpaceId') , uselist=True) 
 
 
+class export(sqlbase):
+    __tablename__ = 'exports'
+    ''' Holder for exported items, 
+    - kept after a session closes 
+    - resulting spaces strictly tracked.
+    '''
 
-class fm_db_user(sqlbase):
-    ...
-class fm_db_tag(sqlbase):
-    ...
-class fm_db_struct_file(sqlbase):
-    ...
+    id        = sqla.Column(sqla.String, primary_key = True)
+    hid       = sqla.Column(sqla.String)
+
+    userId    = sqla.relationship('users',    sqla.ForeignKey('users.id')     ,backref='hasExports')
+    sessionId = sqla.relationship('sessions', sqla.ForeignKey('session.id')   ,backref='hasExports')
+    spaceId   = sqla.relationship('spaces',   sqla.ForeignKey('spaces.id')    ,backref='inExports')
+
+    location  = sqla.Column(sqla.String)
+    data      = sqla.Column(sqla.String)
+
+class view(sqlbase):
+    __tablename__ = 'views'
+    ''' near export duplicate, 
+    - used for temporary work spaces           
+    - always removed after a session completes 
+        - Removes this object, and thus the temp users on the spaces
+    - Resulting spaces are not strictly tracked
+    '''
+
+    id        = sqla.Column(sqla.String, primary_key = True)
+    hid       = sqla.Column(sqla.String)
+
+    userId    = sqla.relationship('users',    sqla.ForeignKey('users.id'))  #,backref='hasViews')
+    sessionId = sqla.relationship('sessions', sqla.ForeignKey('session.id')  ,backref='hasViews')
+    spaceId   = sqla.relationship('spaces',   sqla.ForeignKey('spaces.id')   ,backref='inViews' )
+
+    location  = sqla.Column(sqla.String)
+    data      = sqla.Column(sqla.String)
+
+
+class session(sqlbase):
+    __tablename__ = 'sessions'
+    id     = sqla.Column(sqla.String, primary_key = True)
+    hid    = sqla.Column()
+
+    isOpen
+
+    views   
+    exports 
+
+    perm
+
+    # spaceId   = sqla.relationship('spaces',   sqla.ForeignKey('spaces.id'),  backref='inExports')
+
+class user(sqlbase):
+    __tablename__ = 'users'
+
+
 # class fm_db_interface
