d972ec5fcf441fdf015f8059f7326d7598539719
ClayOfMan <166956516+ClayOfMan@users.noreply.github.com>
2025-07-08 18:06:22 -0400

    testing connection object

--- file_mgmt/client_manager_test/test.py
+++ file_mgmt/client_manager_test/test.py
@@ -1,8 +1,56 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Request
 import functools
 import inspect
 from types import FunctionType
 from typing import Self
+import string
+
+import requests
+
+class connection():
+    ''' 
+    Manages get/post/++ connections with a specific entity.
+    Should return fastapi http exceptions or sucesses
+
+    '''
+    host     : str
+    port     : str
+    subroute : str  #For routing to a specific subpath under the host
+    user     : str|None
+    password : str|None
+
+    def __init__(self,host,port,subroute='',user=None,password=None):
+        self.host     = host
+        self.port     = port
+        self.subroute = subroute
+        self.user     = user
+        self.password = password
+    
+    def _format_kwargs(self,**kwargs):
+        if not kwargs:
+            return ''
+        ret = ''
+        for k,v in kwargs.items():
+            ret.append('?{k}={v}')
+        return '?'+ret
+    
+    def _construct_path(self,subpath,f_kwargs):
+        f_kwargs = self._format_kwargs(f_kwargs)
+        return self.host+':'+self.port+subpath+'?'+f_kwargs
+
+    def get(self,subpath,f_kwargs,**kwargs):
+        assert not kwargs #Custom implimentations may want kwargs
+        path = self._construct_path(subpath,f_kwargs)
+        
+        if self.user: return requests.get(path,auth=(self.user,self.password))
+        else:         return requests.get(path)
+
+    def post(self,subpath,f_kwargs,data,**kwargs):
+        assert not kwargs #Custom implimentations may want kwargs
+        path = self._construct_path(subpath,f_kwargs)
+        
+        if self.user: return requests.post(path,data=data,auth=(self.user,self.password))
+        else:         return requests.post(path,data=data)
 
 class api():
     @classmethod
@@ -62,21 +110,75 @@ class api():
             if self.chain:
                 self.chain.add_api_route(inst,router)
 
+        def filter_fastAPI_params(self,params:dict):
+            #Filter out Request and the like. TODO: Make more generic
+            ret = {}
+            v: inspect.Parameter
+            for k,v in params.items():
+                if v.annotation is not Request:
+                    ret[k] = v
+            return ret
+
+        def format_args_kwargs(self,*args,**kwargs)->tuple[str,dict,dict]:
+            '''Take arguments for the func and format them into path, web pathvars, and leftover kwargs '''
+
+            func_params = dict(inspect.signature(self.func).parameters)
+            func_params = self.filter_fastAPI_params(func_params)
+            
+            k_from_args = {}
+            for x in args:
+                param = func_params.pop[0]
+                assert param.kind != inspect._ParameterKind.KEYWORD_ONLY
+                assert param.name not in kwargs.keys()
+                k_from_args[param.name] = x
+
+            kwargs = kwargs|k_from_args
+
+            path_keys = string.Formatter.parse('',self.path)
+
+            #pop out and use path keys            
+            f_dict = {}
+            for key in path_keys: 
+                f_dict[key] = kwargs.pop[key]
+            path = self.path.format(f_dict)
+
+            #Pop out what are to be path vars            
+            f_kwargs = {}
+            for k,v in kwargs.items: 
+                if k in func_params.keys():
+                    f_dict[k] = kwargs.pop[k]
+
+            #Rest are assumed to be args for the connection objec            
+            r_kwargs = kwargs
+            return path, f_kwargs, r_kwargs
+
+
         def __call__(self,*args,**kwargs):
             raise Exception('_BASE HAS BEEN CALLED')
+
 
     @classmethod
     def get(cls,path, *args,**kwargs):
         cls._wrapper(cls._get,'GET',path, *args, **kwargs)
 
+    @classmethod
+    def post(cls, path, *args,**kwargs):
+        cls._wrapper(cls._post,'POST',path, *args, **kwargs)
+
     class _get(_base):
-        def __call__(self,*args,**kwargs):
-            ''' Call method via connection object or lamda '''
-            raise Exception('NOT PART OF TEST YET')
+        def __call__(self,connection,*args,**kwargs):
+            subpath, f_kwargs, r_kwargs = self.format_args_kwargs(*args,**kwargs)
+            return connection.get(subpath,f_kwargs, **r_kwargs)
+
+    class _post(_base):
+        def __call__(self, connection, data, /, *args,**kwargs):
+            subpath, f_kwargs, r_kwargs = self.format_args_kwargs(*args,**kwargs)
+            return connection.post(subpath,f_kwargs,data=data, **r_kwargs)
 
 
-class Hello:
-    def __init__(self, name: str):
+class Hello:#
+    connection : connection
+    def __init__(self, name: str, connection):
         self.name = name
         self.router = APIRouter()
         api.construct(self,self.router)
@@ -89,14 +191,35 @@ class Hello:
     def hello(self):
         return {"Hello": self.name}
     
-    @api.get(construct_path)
-    def other_func(self):
-        return "NOTHING FOR YOU HERE"
+    @api.get('/who_am_i')
+    def who_am_i(self):
+        return self.name
 
+    @api.get('/echo_test')
+    def echo_connection(self):
+        cls = self.__class__
 
+        return {
+            'FROM_INST' : self.name,
+            'CALLED'    : cls.who_am_i(self.connection)
+            }
+        
+import sys
 app = FastAPI()
-hello = Hello("SomethingNew")
+
+if sys.argv[-1] == 'a':
+    name     = 'Instance_A'
+    settings = {'host':"127.0.0.1", 'port':8003}
+    con      = connection('127.0.0.0', '8002')
+else:
+    name     = 'Instance_B'
+    settings = {'host':"127.0.0.0", 'port':8002}
+    con      = connection('127.0.0.1', '8003')
+
+hello = Hello(name,con)
 app.include_router(hello.router)
+uvicorn.run(app, **settings)
+
+
 
 import uvicorn
-uvicorn.run(app, host="127.0.0.1", port=8002)
