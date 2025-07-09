40fe2487bd5818ec5db4e8bb9a9c8782712bc0a3
[Levi Siewert] <[SiewertLevi@gmail.com]>
2025-07-09 12:25:30 -0400

    started manager client settings sketch

---
+++ file_mgmt/interfaces/fast_api.py
@@ -0,0 +1,29 @@
+from fastapi  import FastAPI, HTTPException, APIRouter
+from pydantic import BaseModel
+
+from .fast_api_base import net_io, api
+
+app = FastAPI()
+
+class Manager(net_io):
+    def __init__(self, name: str, con):
+        self.name = name
+        self.router = APIRouter()
+        self.connection = con
+        api.construct(self,self.router)
+
+    # @api.get('/echo_test')
+    # def echo_connection(self):
+    #     return {
+    #         'FROM_INST' : self.name,
+    #         'CALLED'    : self.api.who_am_i(self.connection)
+    #         }
+
+    @api.put('/file/store')
+    def store_file():
+        ...
+
+    @api.get('/file/i/{file_id}')
+    def get_file_info(file_id):
+        ...
+
--- file_mgmt/interfaces/fast_api_dataclasses.py
+++ file_mgmt/interfaces/fast_api_dataclasses.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+
+
 class header(BaseModel):
     
     ...
