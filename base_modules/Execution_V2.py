ffd6d7dcc4c7720b48cd284a4537681842266a85
[Levi Siewert] <[SiewertLevi@gmail.com]>
2025-08-08 11:17:38 -0400

    removed old & confusing for refactor

--- base_modules/Execution_V2.py
+++ base_modules/Execution_V2.py
@@ -7,170 +7,7 @@ from contextvars      import ContextVar
 from typing import Any
 walk_force = ContextVar('walk_force',default=False)
 
-# class deterministic_chain():
-#     ''' If self & all walking upstream are deterministic, and thus walk_hash is valid to use. '''
-#     class socket_mixin(_mixin.socket):
-#         Deterministic : bool = True
 
-#         @property
-#         def deterministic_chain(self)->bool:
-#             all_determ = []
-
-#             if self.dir in ['in','side']:
-#                 all_determ.append(self.Deterministic)
-#                 for x in self.links:
-#                     all_determ.append(x.in_socket.deterministic_chain)
-                    
-#             elif self.dir in ['out']:
-#                 all_determ.append(self.context.node.deterministic_chain)
-
-#             else: 
-#                 raise Exception('DIRECTION NOT FOUND: ', self.dir)
-
-#             return all(all_determ)
-
-#     class exec_node_mixin(_mixin.exec_node):
-#         Determininistic : bool
-        
-#         @property
-#         def deterministic_chain(self)->bool:
-#             all_determ = []
-#             all_determ.append(self.Deterministic)
-#             for v in self.in_sockets:
-#                 all_determ.append(v.deterministic_chain)
-#             for v in self.side_sockets:
-#                 all_determ.append(v.deterministic_chain) 
-#             return all(all_determ)
-
-
-
-# class walk_hash():
-#     ''' Walk hash, traverse tree to get hashsum of used values used as keys for caches
-#     non-default implimentations may benefit the return as a list of keys 
-#     '''
-
-#     class socket_mixin(_mixin.socket):
-#         _socket_out_is_src_ : bool = False
-#             #Calls value on socket instead of node.
-#             #Edge case
-
-#         value_hash : property
-
-#         @property
-#         def walk_hash(self)->list[str]:
-#             _hash = ''
-
-#             if self.dir in ['in','side']:
-#                 links = [x for x in self.links]
-#                 if len(links):
-#                     for x in links:
-#                         _hash += x.in_socket.walk_hash
-#                 else:
-#                     self.value_hash()
-            
-#             elif self.dir in ['out'] and not self._socket_out_is_src_:
-#                 _hash += self.context.node.walk_hash
-#             elif self.dir in ['out'] and self._socket_out_is_src_:
-#                 _hash += self.value_hash()
-#             else: 
-#                 raise Exception('DIRECTION NOT FOUND: ', self.dir)
-            
-#             return [get_data_uuid(walk_hash)]
-        
-#     class node_mixin(_mixin.exec_node):
-#         @property
-#         def walk_hash(self)->list[str]:
-#             for v in self.in_sockets:
-#                 _hash =+ v.walk_hash
-#             for v in self.side_sockets:
-#                 _hash =+ v.walk_hash 
-#             return [get_data_uuid(_hash)]
-    
-
-# class value_hash():
-#     ''' Localized value hash, invalid if not all inputs are executed (thus could be shorthand, though may be bad practice) '''
-
-#     class node_mixin(_mixin.exec_node):
-#         _node_local_values_ : list[str] = []
-
-#         @property
-#         def value_hash(self)->list[str]:
-#             _pre_sum = []
-#             for k in self._node_local_values_:
-#                 v = getattr(self,k,None)
-#                 if hasattr(v,'value_hash'):
-#                     _pre_sum.append (v.value_hash)
-#                 else:
-#                     _pre_sum.append(get_data_uuid(v))
-
-#             for v in self.in_sockets:
-#                 _pre_sum.append (v.value_hash)
-#             for v in self.side_sockets:
-#                 _pre_sum.append (v.value_hash)
-
-#             if INVALID_UUID in _pre_sum: 
-#                 return INVALID_UUID
-
-#             return [get_data_uuid(''.join(_pre_sum))]
-
-#     class socket_mixin():
-#         executed    : bool = False
-#         _value_hash : str
-
-#         @property
-#         def value_hash(self)->str:
-#             _pre_sum = []
-#             if self.dir in ['in','side']:
-#                 links = [x for x in self.links]
-#                 if len(links):
-#                     for x in links:
-#                         if x.executed:
-#                             _pre_sum.append(x.in_socket.value_hash)
-#                         else:
-#                             _pre_sum.append(INVALID_UUID)
-#                 else:
-#                     _hash = self.get_local_value_hash()
-#                     if _hash != INVALID_UUID:
-#                         self._value_hash = _hash
-#                     return _hash
-                
-#             elif self.dir in ['out']:
-#                 _pre_sum.append(self.get_local_value_hash())
-
-#             else: 
-#                 raise Exception('DIRECTION NOT FOUND: ', self.dir)
-            
-#             if INVALID_UUID in _pre_sum:
-#                 return INVALID_UUID
-            
-#             ret = self._value_hash = get_data_uuid(''.join(_pre_sum))
-#             return ret
-
-#         #The namespace & methodology of this module feels somewhat fragile, or rather; not structural
-        
-#         def get_local_value_hash(self)->str:
-#             a = getattr(self,'data',None)
-#             if hasattr(a,'value_hash'):
-#                 return a.value_hash
-#             else:
-#                 get_data_uuid(a)
-
-
-# class _node_execution():
-#     def __init__(self,func):
-#         self.func = func
-#     def __get__(self, obj, objtype):
-#         return functools.partial(self.__call__, obj)
-    
-#     def __call__(self,container:_mixin.exec_node,*args,**kwargs):
-#         #fullfills and staches caches based on exeuction.
-#         #Depending on mode it could be simpler
-#         # container.Deterministic
-#         return self.func(container,*args,**kwargs)
-
-# def node_exectution(func):
-#     ''' Will expand later to include args such as contextual cache invalidation'''
-#     return _node_execution(func)
 
 from types import UnionType, FunctionType
 from inspect import isclass
@@ -304,45 +141,31 @@ class socket_shapes():
         def Resolve[T](cls,value:T,src_node)->T:
             return cls.Wrap_Funcs(value,src_node)
             
+    ##### Consider making st.func/s return the exec/compile of the upstream socket as a wrapped function! #####
 
-    class func(single):
-        ''' I do question the sanity of this one, as args, kwargs do not raise errors *and* this is a non-uniform interface'''
-        @classmethod
-        def Resolve[T](cls,value:T,src_node)->unlocked_func_container:
-            if isinstance(value,FunctionType):
-                value = cls.Wrap_Func(value,src_node)
-            if not isinstance(value,(unlocked_func_container)):
-                value = cls.Wrap_Func(lambda *args, **kwargs: value,src_node)
-            return value
-    
-    class funcs(multi):
-        ''' I do question the sanity of this one, as args, kwargs do not raise errors *and* this is a non-uniform interface'''
-        @classmethod
-        def Resolve[T](cls,value:T,src_node)->unlocked_func_container:
-            if isinstance(value,FunctionType):
-                value = cls.Wrap_Func(value,src_node)
-            if not isinstance(value,(unlocked_func_container)):
-                value = cls.Wrap_Func(lambda *args, **kwargs: value,src_node)
-            return value
-
-    
-    # class value(single):
-    #     ''' Collapsing to a value on a socket automatically can throw errors with missing arguments. Be aware! '''
-    #     def Resolve[T](value:T,src_node)->T:
-    #         if isinstance(value,(FunctionType,unlocked_func_container)):
-    #             raise Exception('FunctionType was not expected for shape socket')
+    # class func(single):
+    #     ''' I do question the sanity of this one, as args, kwargs do not raise errors *and* this is a non-uniform interface'''
+    #     @classmethod
+    #     def Resolve[T](cls,value:T,src_node)->unlocked_func_container:
+    #         if isinstance(value,FunctionType):
+    #             value = cls.Wrap_Func(value,src_node)
+    #         if not isinstance(value,(unlocked_func_container)):
+    #             value = cls.Wrap_Func(lambda *args, **kwargs: value,src_node)
     #         return value
-        
-    # class values(multi):
-    #     def Resolve[T](value:T)->T:
-    #         if isinstance(value,(FunctionType,unlocked_func_container)):
-    #             value = value()
+    
+    # class funcs(multi):
+    #     ''' I do question the sanity of this one, as args, kwargs do not raise errors *and* this is a non-uniform interface'''
+    #     @classmethod
+    #     def Resolve[T](cls,value:T,src_node)->unlocked_func_container:
+    #         if isinstance(value,FunctionType):
+    #             value = cls.Wrap_Func(value,src_node)
+    #         if not isinstance(value,(unlocked_func_container)):
+    #             value = cls.Wrap_Func(lambda *args, **kwargs: value,src_node)
     #         return value
-
-    _single = [single,func]
-    _multi  = [multi,funcs]
 
 st = socket_shapes
+
+
 
 class main(module):
     UID          = 'Core_Execution'
@@ -353,13 +176,10 @@ class main(module):
 
     class socket_mixin(_mixin.socket):
         
-        Value_Shape   : st.mutable = st.mutable
-            #Statement to make inputs typed single or multi
-            #Autommatic mutable
-        Value_Type    : Any|set[Any]|UnionType
-            #Statemnt of type(s) produced by socket.
-        Value_Default : Any = _unset
-            #Not saved to file.
+        Value_Shape   : st.mutable = st.mutable # Statement to make assert shapes/forms
+                                                
+        Value_Type    : Any|set[Any]|UnionType  # Statemnt of type(s) produced by socket.
+        Value_Default : Any = _unset            # Not saved to file.
         Value_Strict  : bool = True
 
         def Value_Verify(self,value):
@@ -401,52 +221,6 @@ class main(module):
         Out_Value_Resolution_Chain = {'value_graph'}
             #these could be a property for custom things that connect to UI
 
-        @property
-        def value(self):
-            if self.Direction in ['in','side']:
-                attr_chain = self.In_Value_Resolution_Chain
-            else:
-                attr_chain = self.Out_Value_Resolution_Chain
-
-            for attr in attr_chain:
-                if val:=getattr(self,attr) is not _unset:
-                    return val
-            return _unset
-            
-        # @value.setter
-        # def value(self,value):
-        #     ''' Value can only be set on output nodes. 
-        #     This could be where the execution cache is stored under the context ID of the node? '''
-        #     assert self.Direction == 'out'
-        #     self._value = value
-        
-        _value         : Any = _unset
-        _value_graph   : Any = _unset
-        _value_user    : Any = _unset
-        value_default  : Any = _unset
-
-        @property
-        def value_user(self):
-            return self._value_user 
-        
-        @value_user.setter
-        def value_user(self,value):
-            self._value_user = value 
-
-        @property
-        def value_graph(self):
-            ''' Auto execution property '''
-            if self.Direction in ['in','side']:
-                return self.Value_Shape.get(self)
-            else:
-                val = self.value_exec
-                if val is _unset:
-                    self.context.node.execute()
-                val = self.value_exec
-                if val is _unset:
-                    raise Exception('SOCKET value_exec WAS NOT SET DURING EXECUTION')
-                return val
-
 
 
     class node_mixin(_mixin.node):
@@ -462,22 +236,15 @@ class main(module):
             # Values to invalidate disc/mem cachable inherited from the output sockets
             # Check against all output sockets being disc cachable to verify. Otherwise throw an error.
 
-
-        
         disc_cached   : bool = False
         disc_location : str
+            #wrapper will utilize this
+
+
 
 
     class exec_node_mixin(_mixin.exec_node):
-        def execute(self):
-            ''' Exeuction Function '''
-            raise Exception(f'Execution has not been defined on {self.UID} of ({self.module.UID} : {self.module.Version}) !')
-
-        def execute_inputs(self,):
-            ''' Calls execute on all inputs '''
-            for x in self.in_sockets:
-                ...
-
+        def execute(): ...
 
     class placeholder_node_mixin(_mixin.exec_placeholder_node):
         def execute(): ...
