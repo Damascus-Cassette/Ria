bc42eda4e96dfc1b3f27f50063468bf9133c3816
[Levi Siewert] <[SiewertLevi@gmail.com]>
2025-08-20 13:57:22 -0400

    cont working on hooks and wrapper system

--- models/struct_hook_base_new.py
+++ models/struct_hook_base_new.py
@@ -47,11 +47,12 @@ class _shared_class():...
 class _hook(_shared_class):
     def __init__(self ,
                 func        :FunctionType|Self ,
+                /,
                 event       :str               ,
-                /,*,
+                *,
                 key         :str    = None     ,
                 anon        :bool   = None     ,
-                mode        :str    = 'post'   ,
+                mode        :str    = None     ,
                 see_args    :bool   = False    ,
                 passthrough :bool   = False    ,
                 ):
@@ -65,6 +66,7 @@ class _hook(_shared_class):
             self.event_siblings  = []
             self.hooked_siblings = []
         self.hook_siblings.append(self)
+        #Modes must be uniform, inherit if none and raise if different in chain.
         self.func        = func
         self.event       = event
         self.key         = key  if  key          else (func.__module__ + func.__name__)
@@ -72,6 +74,15 @@ class _hook(_shared_class):
         self.mode        = mode
         self.see_args    = see_args
         self.passthrough = passthrough
+        if passthrough and not see_args: 
+            raise Exception('HOOK SYSTEM: Passthrough must have see_args enabled') 
+        
+        _modes = list(set([x.mode for x in self.hook_siblings if x.mode is not None]))
+        if len(_modes)>1:
+            raise Exception('HOOK SYSTEM: hook chain modes are not uniformly!')
+        elif len(_modes)==1:
+            for x in self.hook_siblings:
+                x.mode = _modes[0]
 
     def __call__(self, *args, **kwargs):
         #May require __get__ with functools.partial to get container iirc
@@ -86,11 +97,23 @@ class _hook(_shared_class):
             return wraps(func)(cls(func,**kwargs))
         return wrapper
 
-    def __enter__(self,):
-        ...
-    def __exit__():
-        ...
+    def run(self,args,kwargs):
+        assert self.mode is not None
+        assert self.mode != 'context'
 
+        if self.see_args and (self.passthrough or self.mode == 'wrap'):
+            res =  self.func(*args,**kwargs)
+            assert res is not None
+            return res
+        elif self.see_args:
+            self.func(*args,**kwargs)
+            return args,kwargs
+        else: 
+            self.func()
+        
+    def return_context_generator_object(self,*args,**kwargs):
+        return _enter_exit_hidden(self.func(*args,**kwargs))
+    
 class _hooked(_shared_class):
     def __init__(self,
                  func:FunctionType|Self ,
@@ -109,12 +132,15 @@ class _hooked(_shared_class):
         self.func  = func
         self.event = event
 
-    def __call__(self,*args,**kwargs):
+    def __call__(self,container,*args,**kwargs):
         #May require __get__ with functools.partial to get container iirc
         #header is called, not a middle stack item.
-        container : Hookable = args[0]
-        return container._hooks.run_with_hooks(self,args,kwargs)
-        
+        # container : Hookable = args[0]
+        return container._hooks.run_with_hooks(self,container,self.func,args,kwargs)
+    
+    def __get__(self,instance,owner):
+        return partial(self.__call__,instance)
+
     @classmethod
     def wrapper(cls,**kwargs):
         def wrapper(func):
@@ -135,6 +161,7 @@ class _def_dict_dict(dict):
         self[key] = ls
         return ls
 
+from functools import partial
 class hook_group():
     ''' Namespace and Anon merging on inheritance via a construction function, plus execution of hooks '''
     
@@ -187,8 +214,8 @@ class hook_group():
         ''' Run a hooked function, runs with the hooks ascociated (also runs events on obj) '''
         #Run hooked, run hooks, post events, then exit context
         with _validate_op_chain(container,hooked_inst.event):
-            hooks = copy(self.anon_hooks[hooked.event])
-            hooks.append(*self.named_hooks[hooked.event].values())
+            hooks = copy(self.anon_hooks[hooked_inst.event])
+            hooks.extend(list(self.named_hooks[hooked_inst.event].values()))
                 #Retrieve hooks
             events = hooked_inst.event_siblings
                 #Retrieve events
@@ -198,49 +225,54 @@ class hook_group():
 
             for x in cache:
                 #TODO: how should cache retrieval interact with events?
-                res = x.run(args,kwargs)
+                res = x.run(container, args,kwargs)
                 if res is not _unset:
                     return res
 
             _context = []
             for x in context:
-                gen = x.return_context(*args,**kwargs)
+                gen = x.return_context_generator_object(*args,**kwargs)
                 args,kwargs = gen.__enter__()
                 _context.append(gen)
 
             for x in pre:
-                args,kwargs = x.run(args,kwargs)
+                args,kwargs = x.run(container, args,kwargs)
             
             func = hooked_inst.func
             for x in wrap:
-                func = x.run(func,*args,*kwargs)
+                func = x.run(container, func,*args,*kwargs)
                 assert isinstance(func,FunctionType)
             
-            res = func(*args,**kwargs)
+            res = func(container,*args,**kwargs)
 
             for x in post:
-                res = x.run(res)
+                res = x.run(container, res)
 
             for x in events:
-                x.run()
+                x.post(container, hooked_inst.event, func, args, kwargs, res)
 
             for x in _context[-1:]:
                 x.__exit__()
 
         return res
 
-        #TODO
+    def run_as_hook(self, hook_inst:_hook, args, kwargs):
+        ''' Hooks are run without triggering hooked atm, Limits the possiblity of recursion '''
+        # ''' Runs a function via it's hook (also runs events on obj & calls hooked w/a + throw error with recursive event triggering) '''
+        #TODO: Maybe run downstream hooked and post events?
 
-    def run_as_hook(self, hook_inst:_hook, container, func, args, kwargs):
-        ''' Runs a function via it's hook (also runs events on obj & calls hooked w/a + throw error with recursive event triggering) '''
-        #Run hook's func, run hooked, post events, then return
-        #TODO
+        return hook_inst.func(*args,**kwargs)
+
+    # def __get__(self, instance, owner):
+    #     if instance is None: 
+    #         return self
+    #     return partial(self,instance)
     
     def split_hooks_to_modes(self,hooks)->tuple[list[_hook]]:
         items = _def_dict()
         for x in hooks:
             items[x.mode].append(x)
-        return items['pre'], items['post'], items['wrap'], items['context']
+        return items['cache'],items['pre'], items['post'], items['wrap'], items['context']
         
 
 class Hookable():
@@ -253,7 +285,7 @@ class Hookable():
             hg.merge(a)
         cls._hooks = hg
 
-        for k,v in cls.__dict__:
+        for k,v in cls.__dict__.items():
             if issubclass(v.__class__,_shared_class):
                 hg.intake(v)
 
@@ -262,7 +294,7 @@ class Hookable():
 # def event_subscribe():...
 
 from contextvars import ContextVar
-if 0:
+if __name__ == '__main__':
 
     add_me = ContextVar('addme',default=1)
     class mixin():
@@ -270,9 +302,10 @@ if 0:
               key         = 'event_hook_1', #Non-Anonomous key for replacing this function. Required if anon, not anon recorded as module + funcname
               anon        = False,          #If a hook is bound to a namespace or not for replacing it
               mode        = 'post',         #Timing of function (cache, pre, post, wrap, context)
-              see_args    = False,          #See args & kwargs or not, default = False
-              passthrough = True,)          #If the values will be passed through, which allows for inline changes pre and post.
+              see_args    = True,          #See args & kwargs or not, default = False
+              passthrough = True,)         #If the values will be passed through, which allows for inline changes pre and post.
         def add_c_to_res(self,result):
+            print('CALLED ME')
             return result + add_me.get()
         
         @hook(event = 'event_1',
@@ -286,7 +319,15 @@ if 0:
     class base(mixin, Hookable):
         
         @hooked(event = 'event_1')
-        def func(self,value:int):
+        def func(self,value:int=None):
             return value
         
-    assert 4 == base().func(1)
+    from pprint import pprint
+ 
+    pprint(base._hooks.anon_hooks)
+    pprint(base._hooks.named_hooks)
+    pprint(base._hooks.hooked)
+
+    result =  base().func(1)
+    print(result)
+    assert 4 == result
