39bc6929c8e80b74fed82c43b4943566bfa0a51c
[Levi Siewert] <[SiewertLevi@gmail.com]>
2025-09-03 13:01:53 -0400

    Added Submodule_Cache

---
+++ base_modules/RenderFarm_1_0/Submodule_Cache.py
@@ -0,0 +1,200 @@
+from pydantic import BaseModel as pydantic_Basemodel
+from enum import Enum
+from inspect import isgeneratorfunction
+class cache_item_location_state(Enum):
+    LOCAL_MEM   = 'LOCAL_MEM'  
+    LOCAL_DISC  = 'LOCAL_DISC' 
+    MANAGER     = 'MANAGER'    
+    SIBLING     = 'SIBLING'    
+
+class cache_item_local_state(Enum):
+    AVALIABLE   = 'AVALIABLE'
+    UNAVALIABLE = 'UNAVALIABLE'
+    DEPRECIATED = 'DEPRECIATED'
+
+class cache_item_manager_state(Enum):
+    NOT_UPLOADED = 'NOT_UPLOADED'
+    AVALIABLE    = 'AVALIABLE'
+    UNAVALIABLE  = 'UNAVALIABLE'
+    DEPRECIATED  = 'DEPRECIATED'
+    #AVALIABLE_FOREIGN 
+
+class Cache_Item_Reference(pydantic_Basemodel): 
+    # Single data-type meant to be frequently synced. Good use case for pydantic instead of internal fileio.BaseModel
+    # Referes to the state of a single cache item which may or may not be local
+
+    cache_uuid    : str
+    manager_state : cache_item_manager_state
+
+    _loc_state  : cache_item_location_state # Relative information for current thread 
+    _state      : cache_item_local_state    
+    local_only  : bool
+
+class Cache_Itenerary[Cache_Item_Reference](dict):
+    ''' May make this a synced datastructure '''
+
+
+class Cache_Item(BaseClass):
+    ''' Loaded from disc, mem or manager->mem. 
+    Contains data to apply to a node on cache retrieval. Allows extra data as a generic.
+    `extra_data` best used for controlled validation inside a func before yielding/returning or handled inside of yield & post
+    Usual best practice with yield is to always locally ascociate the `state_key` with a cache in the wrapper, 
+        so an exec function itself should only run once w/a
+    '''
+
+    data_uid    : str                       #UID of this data 
+
+    out_sockets : dict[str, dict[str, Any]] #Socket name/index, then attributes that are intaken. 
+    extra_data  : dict                      #Generic data to be handled by the node itself
+
+    def __init__(self,):
+        self.out_sockets = {}
+        self.extra_data  = {}
+
+    def set_uid(self,):
+        assert not getattr(self.data_uid)
+        key =  uuid_hash_from_data(self.out_sockets)
+        key =+ uuid_hash_from_data(self.extra_data)
+        self.data_uid = key
+
+    # def references(self,Cache):...
+
+current_cache           = ContextVar('CurrentCache', default = None) 
+cache_future_asc_public = ContextVar('cache_future_asc_public', default = None)
+cache_future_asc_local  = ContextVar('cache_future_asc_local', default = None)
+class Cache():
+    ''' Shallow container for all cache_items, handles cache retrieval w/a '''
+
+    itenerary : Cache_Itenerary
+    data      : dict[Cache_Item]
+    
+    def __init__(self,):
+        self.itenerary = Cache_Itenerary()
+        self.caches    = {}
+
+    # def sync_item():
+    #     ''' sync item from the manager class '''
+
+
+    @classmethod
+    def create_wrapper(self,func):
+        if isgeneratorfunction(func):
+            return self.value_cache_gen_wrapper(func)
+        return self.value_cache_reg_wrapper(func)
+
+    @staticmethod
+    def value_cache_reg_wrapper(func):
+        ''' Wrapper for Single-Step-Execution Function. Self is Node 
+    
+        Runs when state_key does not pass muster for current, and when memo does not intake.
+    
+        currently goes through these phases:
+            node executes -> uncached & local -> cached & local -> cached ascociated & local
+        
+
+        '''
+        def wrapper(self,*args,**kwargs):
+            
+            state_key = self.state_key
+            if self.still_need_to_execute_or_cache(state_key):
+                return None
+            
+            global cache_future_asc_public
+            global cache_future_asc_local
+            t1 = cache_future_asc_public.set([])
+            t2 = cache_future_asc_local .set([])
+
+
+
+            self.asscociate_key(state_key)
+
+            if (res:=self.find_cache(state_key)) is _unset:
+                #execute if not in cache            
+                res = func(*args,**kwargs)
+
+            if isinstance(res,Cache_Item):
+                cache_item = res
+                res = self.intake_cache(cache_item)
+
+
+            elif self.create_cache:
+                cache_item = self.create_cache()
+                global current_cache
+                current_cache.get().add_cache(cache_item)
+            
+            self.create_cache_asc(cache_item)
+
+            cache_future_asc_public.reset(t1)
+            cache_future_asc_local .reset(t2)
+
+            return res
+
+        return wrapper
+        
+    @staticmethod
+    def value_cache_gen_wrapper(func):
+        ''' Wrapper for generator-Execution Function.  Self is Node.
+        `yield` in func will consider function complete with first `not (yield_res is _unset)`
+            - This includes `return None` and `yield None`
+
+        Runs when state_key does not pass muster for current, and when memo does not intake.
+
+        '''
+        def wrapper(self,*args,**kwargs):
+            self:node
+            
+            state_key = self.state_key
+            if self.still_need_to_execute_or_cache(state_key):
+                return None
+
+            global cache_future_asc_public
+            global cache_future_asc_local
+            t1 = self.cache_future_asc_public.set([])
+            t2 = self.cache_future_asc_local .set([])
+
+            if (res:=self.find_cache(state_key)) is not _unset:
+                self.intake_cache(res)
+            else:
+                #execute if not in cache            
+                self.asscociate_key(state_key)
+                gen = func(*args,**kwargs)
+
+                for res in gen:
+                    if res is _unset:
+                        continue
+
+                    if isinstance(res, Cache_Item):
+                        cache_item = res
+                        res = self.intake_cache(cache_item)
+                        break
+
+                    if self.create_cache:
+                        cache_item = self.create_cache()
+                        global current_cache
+                        current_cache.get().add_cache(cache_item)
+                    break
+                if res is _unset:
+                    raise Exception(f'A resulting value cannot be unset! {self.__module__}.{self.__name__} : {func}')
+                
+            self.create_cache_asc(cache_item)
+
+            cache_future_asc_public.reset(t1)
+            cache_future_asc_local .reset(t2)
+
+            return res
+
+        return wrapper
+
+class Cache_IO():
+    ''' Inherited method class on each node for ease of access to caching functionality '''
+    
+    def cache_search(self,):        
+        ...
+
+    def intake_cache():
+        ...
+    def add_cache():
+        ...
+    def asscociate_key(self,key:str):
+        ''' Ascociates key with potential future cache '''
+        ...
---
+++ base_modules/RenderFarm_1_0/Submodule_StateKey.py
@@ -0,0 +1,180 @@
+Backwards_Context()
+
+from typing import TypeAlias
+
+
+dep_keys   : TypeAlias = tuple
+struct_key : TypeAlias = str
+
+local_state_key      : TypeAlias = str
+
+contextual_state_key : TypeAlias = str
+
+
+class socket():
+
+    _struct_key   : struct_key
+    _context_deps : dep_keys
+
+    @property
+    def state_key(self):
+        return self.context.node.state_key
+
+    def local_state_key(self)->local_state_key:
+        ''' Local state of this socket in a hashable form. 
+        Does not include links (part of structural), 
+        But local data can change in relation to links' presence (ie unused Default_Value)
+            - This is the default implamentation assumption 
+        Best way to think about this is uuid_hash(all used values)
+        '''
+        
+        if self.direction.upper != 'OUT':
+            if len(self.links):
+                return 'UPSTREAM' 
+                #As noted, by default link_values flatten local_state.
+            return uuid_from_value(self.Default_Value) 
+                #May want to use resolution chain
+        
+        return self.label
+
+        #Above MAY need to be more unique, though I think the stuructral access is OK
+
+    def init_state_components_outsockets(self):
+        ''' Called on out sockets, forwards quiry to node (which observes local state)
+            - ~note: this method causes a structural_key change if a sibling socket's local_state_key is different.~
+                - UPDATE: preventing this by not adding output socket's state directly to the node's structural state
+                - This may create different problems later in specific circustamnces
+                    - ie when a node's outputs affect each socket's execution
+                    - In situations like above, the output's affects should be factored into the **node's** local_state_key 
+        '''
+        assert self.direction.upper() == 'OUT'
+        struct_key, deps =  self.context.node.init_state_components()
+        struct_key = struct_key + self.local_state_key() + self.direction
+        
+        deps = tuple(sorted(list(set(deps))))
+        struct_key = uuid_hash(struct_key)
+
+        self._struct_key = struct_key
+        self._context_deps = deps
+
+        return struct_key, deps
+        
+
+    def init_state_components_insockets(self)->tuple[struct_key,dep_keys]:
+        ''' Called on in/side sockets, gets local socket and passing along quiry  '''
+        assert self.direction.upper() != 'OUT'
+        for x in self.links:
+            _struct_key, _deps = x.link.out_socket.init_state_components_outsockets()
+        
+        struct_key =+ _struct_key
+        deps =+ _deps
+
+        deps = tuple(sorted(list(set(deps))))
+        struct_key = uuid_hash(struct_key)
+
+        self._struct_key = struct_key
+        self._context_deps = deps
+
+        return struct_key, deps
+
+
+class socket_collection():
+
+    # def local_state_key(self)->local_state_key:
+    #     ''' called on out_sockets only '''
+    #     local_state_key = ''
+
+    #     for socket in self:
+    #         local_state_key =+ socket.local_state_key
+
+    #     return uuid_hash(local_state_key)
+
+    def init_state_components(self)->tuple[struct_key,dep_keys]:
+        assert self.direction.upper() != 'OUT'
+        
+        struct_key = ''
+        deps = tuple()
+        for socket in self:
+            _struct_key, _deps = socket.init_state_components_insockets()
+            struct_key =+ _struct_key
+            deps =+ _deps
+
+        deps = tuple(sorted(list(set(deps))))
+        struct_key = uuid_hash(struct_key)
+
+        # self.struct_key = struct_key
+        # self.deps = deps
+
+        return struct_key, deps
+
+
+class node():
+    _struct_key   : struct_key = _unset
+    _context_deps : dep_keys   = _unset
+
+    @property
+    def state_key(self):
+        if self._struct_key is _unset:
+            self.init_state_components()
+
+        state_key = self._struct_key
+            #seeding state_key
+        
+        c_keys = [*Backwards_Context.keys()]
+        for key in self._context_deps:
+            if key not in c_keys  :
+                raise Exception(f'Unconstrained Context Error with key: {key}, node: {self.context.Repr()}', )
+
+            val = Backwards_Context[key].get()
+
+            if val is _unset:
+                raise Exception(f'Unset Context Error with key: {key}, node: {self.context.Repr()}', )
+
+            state_key =+ uuid_from_value(val)
+
+        return uuid_hash(state_key)
+
+    def local_state_key(self)->local_state_key:
+        ''' Get this node's local state key, local untracked values that affect execution into a hashable format.
+        Does not include sockets and links (contributers of struct_key)
+        Exceptions to Change:
+            - When values that are local to the node are **not** in sockets
+            - When output sockets's affect execution
+        '''
+        return 'DEFAULT'
+
+    def init_state_components(self,)->tuple[struct_key,dep_keys]:
+        #Structurally, what is this key backwards_context vars does this depend on
+        local_state = self.local_state_key()
+        
+        struct_key = local_state
+        deps = tuple()
+
+        _struct_key, _deps = self.in_sockets.init_state_components()
+        struct_key =+ _struct_key
+        deps =+ _deps
+        
+        _struct_key, _deps = self.side_sockets.init_state_components()
+        struct_key =+ _struct_key
+        deps =+ _deps
+
+        # _struct_key = self.out_sockets.local_state_key()
+        # struct_key =+ _struct_key
+        # # deps =+ _deps
+
+        deps = self.change_context_dep_keys(tuple(sorted(list(set(deps)))))
+        struct_key = uuid_hash(struct_key)
+
+        self._struct_key   = struct_key
+        self._context_deps = deps
+
+        return struct_key, deps
+    
+    def change_context_dep_keys(deps:dep_keys)->dep_keys:
+        ''' Contextual add/remove dependencies from passing downstream
+        Zone-Start should add    a key based on the zone set
+        Zone-End   should remove a key based on the zone set
+            - Context variables should not be unconstrained forward past the spawner.
+            - Compilation is where the Backwards_Context is actuallly changed (as state_key is accessed in compilation & execution)
+        '''
+        return deps
